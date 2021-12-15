8d5d56fa12ffc339b9268990d47a66d068b5b067
ManuelExposito <manuelexposito4d@gmail.com>
2021-12-15 12:36:29 +0100

    comienzo actividad authguard

--- gasapp/src/app/modules/app-routing.module.ts
+++ gasapp/src/app/modules/app-routing.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from '../components/shared/sign-in/sign-in.component';
 import { GasolineraListComponent } from '../pages/gasolinera-list/gasolinera-list.component';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+
+
+
 
 const routes: Routes = [
 
   {path:'',  pathMatch:'full', component:SignInComponent},
   {path:'gasolineras', component:GasolineraListComponent},
-  {path: 'gasolinerasFav', component:GasolinerasFavoritasComponent}
+  {path: 'gasolinerasFav', canActivate:[AngularFireAuthGuard], component:GasolinerasFavoritasComponent}
   //{path:'', pathMatch:'full', redirectTo:'/sign-in'}
 
 ];
